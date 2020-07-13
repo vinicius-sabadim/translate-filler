8125b76b0d74bb7c3d771d343cc5c16d73782f56
vinicius-sabadim <vinicius.sabadim@gmail.com>
2020-07-12 21:11:42 -0300

    Include missing language in keys

--- check-languages.py
+++ check-languages.py
@@ -27,6 +27,15 @@ def separate_keys_with_language(files):
     return my_dict
 
 
+def insert_missing_language(data, languages):
+    for key in data:
+        for language in languages:
+            language = language.replace(".json", "")
+            if not language in data[key]:
+                data[key][language] = ""
+    return data
+
+
 def save_json(data):
     with open("language.json", "w") as output_file:
         output_file.write(data)
@@ -36,5 +45,6 @@ language_files = [f for f in os.listdir(
     '.') if os.path.isfile(f) and is_valid_file(f)]
 
 file = separate_keys_with_language(language_files)
+file = insert_missing_language(file, language_files)
 final_json = json.dumps(file, sort_keys=True, indent=2)
 save_json(final_json)
